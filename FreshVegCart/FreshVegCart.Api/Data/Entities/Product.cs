1fc07a63c68393f90e4908d60eb2d60a43f008a7
Michael Bond <michael@bondcodes.com>
2025-02-12 09:11:34 -0500

    Add Product / User

---
+++ FreshVegCart/FreshVegCart.Api/Data/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreshVegCart.Api.Data.Entities;
+
+public class Product
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required, MaxLength(50)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required, MaxLength(200)]
+    public string ImageUrl { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+
+    [Required, MaxLength(10)]
+    public string Unit { get; set; } = string.Empty;
+}
---
+++ FreshVegCart/FreshVegCart.Api/Data/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreshVegCart.Api.Data.Entities;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required, MaxLength(20)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required, MaxLength(150)]
+    public string Email { get; set; } = string.Empty;
+
+    [MaxLength(20)]
+    public string? Phone { get; set; }
+
+    [Required]
+    public string PasswordHash { get; set; } = string.Empty;
+}
